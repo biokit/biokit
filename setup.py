1aa37739cc780ad02c85f956369f01c387b457f8
Thomas Cokelaer <cokelaer@gmail.com>
2014-09-09 18:56:42 +0100

    add new files.

---
+++ setup.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+__revision__ = "$Id$"
+import sys
+import os
+from setuptools import setup, find_packages
+import glob
+
+
+_MAJOR               = 0
+_MINOR               = 0
+_MICRO               = 1
+version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
+release              = '%d.%d' % (_MAJOR, _MINOR)
+
+metainfo = {
+    'authors': {
+        'Cokelaer':('Thomas Cokelaer','cokelaer@gmail.com'),
+        },
+    'version': version,
+    'license' : 'BSD',
+    'download_url' : ['http://pypi.python.org/pypi/biokit'],
+    'url' : ['http://pypi.python.org/pypi/biokit'],
+    'description':'Access to Biological Web Services from Python' ,
+    'platforms' : ['Linux', 'Unix', 'MacOsX', 'Windows'],
+    'keywords' : [],
+    'classifiers' : [
+          'Development Status :: 5 - Development',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+          'Topic :: Scientific/Engineering :: Bio-Informatics',
+          'Topic :: Scientific/Engineering :: Information Analysis',
+          'Topic :: Scientific/Engineering :: Mathematics',
+          'Topic :: Scientific/Engineering :: Physics']
+    }
+
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+
+setup(
+    name             = 'biokit',
+    version          = version,
+    maintainer       = metainfo['authors']['Cokelaer'][0],
+    maintainer_email = metainfo['authors']['Cokelaer'][1],
+    author           = metainfo['authors']['Cokelaer'][0],
+    author_email     = metainfo['authors']['Cokelaer'][1],
+    long_description = readme,
+    keywords         = metainfo['keywords'],
+    description = metainfo['description'],
+    license          = metainfo['license'],
+    platforms        = metainfo['platforms'],
+    url              = metainfo['url'],      
+    download_url     = metainfo['download_url'],
+    classifiers      = metainfo['classifiers'],
+
+    # package installation
+    #package_dir = {'':''},
+    packages = ['biokit'],
+    #package_dir  = package_dir,
+    install_requires = [],
+    )
+
+
---
+++ test/test_biokit.py
@@ -0,0 +1,5 @@
+import biokit
+
+
+def test_biokit():
+    import biokit
