75a934d15073a45257a72cfe67e430d6e7edd911
Thomas Cokelaer <cokelaer@gmail.com>
2014-10-08 23:02:56 +0100

    fixing colorbar + combination of upper/lower triangles

--- biokit/viz/corrplot.py
+++ biokit/viz/corrplot.py
@@ -1,66 +1,77 @@
+import string
+
+from colormap import cmap_builder
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 from matplotlib.patches import Ellipse, Circle, Rectangle, Wedge
-import string
-
+from matplotlib.collections import PatchCollection
+import pandas as pd
 """
+
 references: http://cran.r-project.org/web/packages/corrplot/vignettes/corrplot-intro.html
 
 
 todo:: 
 
  - addrect if clustering
- - colorbar
 
 """
 
 class Corrplot(object):
+    """A implementation of correlation plotting (corrplot)
+
+    Input must be a dataframe (Pandas) with data (any values) or a correlatoin 
+    matrix (square) with values between -1 and 1.
+       
+    If NAs are found in the correlation matrix, there are replaced with zeros. 
+
+    Data can also be a correlation matrix as a 2-D numpy array containing
+    the pairwise correlations between variables. Labels will then be numerical
+    indices though. 
+
+
+    By default from red por positive correlation to blue for negative ones but other colormap
+    can easily be provided.
+
+        # standard matplotlib cmap
+        c.plot(method='square', cmap='jet')
+        # or 2 colors
+        c.plot(method='square', cmap=('red','green'))
+        # or 3 colors
+        c.plot(method='square', cmap=('red','white','green'))
+
     """
-
-    Input must be a correlation matrix in pandas format with a square matrix and index and columns
-    names identical. If not, we assume that the input data is not a a correlation matrix. A correlation is therefore computed in such case. 
-
-    data could also be a correlation matrix, a 2-D numpy array containing
-    the pairwise correlations between variables. 
-
-    pvalues is a matrix containing the pvalue for each corresponding
-    correlation value; if none it is assumed to be the zero matrix
-
-
-    more options with the clustering/order + rectangle ?
-
-    TODO
-
-    values could be non-correlation between -100 and 100
-    values could be confidence interval ??
-    significance test: values above or below some threshold could be crossed or left blank (e.g. set to 0)
-    """
-    def __init__(self, data, pvalues=None):
+    def __init__(self, data, pvalues=None, na=0):
         #input data can be a dataframe only
-        self.df = data.copy()
-        w, h = self.df.shape 
+        #try:
+        self.df = pd.DataFrame(data, copy=True)
+        #except:
+        #    self.df = pd.DataFrame(data)
+
+        compute_correlation = False
+        w, h = self.df.shape
+        if self.df.max().max() >1 or self.df.min().min()<-1:
+            compute_correlation = True
+        if w !=h:
+            compute_correlation = True
+        if list(self.df.index) != list(self.df.columns):
+            compute_correlation = True
+
+        if compute_correlation:
+            print("Computing correlation")
+            cor = self.df.corr()
+            self.df = cor
+
+        # replace NA with zero
+        self.df.fillna(na, inplace=True)
+
+        self.params = {'colorbar.N': 100}
         
-        
-        if w !=h or list(self.df.index) != list(self.df.columns):
-            try:
-                print("Computing correlation")
-                cor = self.df.corr()
-                self.df = cor
-                w, h = self.df.shape
-            except:
-                raise ValueError
 
-
-        if pvalues is None:
-            self.pvalues = np.zeros([w, h])
-        else:
-            self.values = values
-
-        self.poscm = cm.get_cmap('Blues')
-        self.negcm = cm.get_cmap('Oranges')
-        self.negcm = cm.get_cmap('Reds')
-
+    def _set_default_cmap(self):
+        self.cm = cmap_builder('#A80000','white','darkblue')
 
     def order(self, method='complete', metric='euclidean',inplace=False):
         import scipy.cluster.hierarchy as hierarchy
@@ -74,15 +85,28 @@ class Corrplot(object):
         cor2 = self.df.ix[idx1][idx1]
         if inplace is True:
             self.df = cor2
-
         else:
             return cor2
-        
 
     def plot(self, num=1, grid=True,
-            rotation=30, colorbar_width=10, fill='both', lower=None, upper=None, 
-            shrink=0.9, axisbg='white', colorbar=False, label_color='black',
-            fontsize='small', edgecolor='black', method='ellipse', order=None):
+            rotation=30, colorbar_width=10, lower=None, upper=None, 
+            shrink=0.9, axisbg='white', colorbar=True, label_color='black',
+            fontsize='small', edgecolor='black', method='ellipse', order=None,
+            cmap=None
+            ):
+
+        # default
+        if cmap != None:
+            try:
+                if isinstance(cmap, str):
+                    self.cm = cmap_builder(cmap)
+                else:
+                    self.cm = cmap_builder(*cmap)
+            except:
+                print("incorrect cmap. Use default one")
+                self._set_default_cmap()
+        else:
+            self._set_default_cmap()
 
         self.shrink = shrink
         self.fontsize = fontsize
@@ -96,32 +120,46 @@ class Corrplot(object):
         plt.clf()
         fig = plt.figure(num=num, facecolor=axisbg)
 
-        if colorbar:
-            ax = plt.subplot(1, 2, 1, aspect='equal')
-            axc = plt.subplot(1, 2, 2)
-        else:
-            ax = plt.subplot(1, 1, 1, aspect='equal', axisbg=axisbg)
+        ax = plt.subplot(1, 1, 1, aspect='equal', axisbg=axisbg)
         # subplot resets the bg color, let us set it again
         fig.set_facecolor(axisbg)
-
 
         width, height = df.shape
         labels = (df.columns)
 
-        self.lower = lower
-        self.upper = upper
-        if self.lower is not None:
-            self._fill_triangle(df, lower, 'lower',  ax)
-        if self.upper is not None:
-            self._fill_triangle(df, upper, 'upper', ax)
-        if self.upper is None and self.lower is None:
-            self._fill_triangle(df, method, fill,  ax)
+        # add all patches to the figure
+        # TODO check value of lower and upper
         
+        if upper is None and lower is None:
+            mode = 'method'
+            diagonal = True
+        elif upper and lower:
+            mode = 'both'
+            diagonal = False
+        elif lower is not None:
+            mode = 'lower'
+            diagonal = True
+        elif upper is not None:
+            mode = 'upper'
+            diagonal = True
+        else:
+            raise ValueError
+       
+        if mode == 'upper':
+            self._add_patches(df, upper, 'upper',  ax, diagonal=True)
+        elif mode == 'lower':
+            self._add_patches(df, lower, 'lower',  ax, diagonal=True)
+        elif mode == 'method':
+            self._add_patches(df, method, 'both',  ax, diagonal=True)
+        elif mode == 'both':
+            self._add_patches(df, upper, 'upper',  ax, diagonal=False)
+            self._add_patches(df, lower, 'lower',  ax, diagonal=False)
 
-
+        # shift the limits to englobe the patches correctly
         ax.set_xlim(-0.5, width-.5)
         ax.set_ylim(-0.5, height-.5)
             
+        # set xticks/xlabels on top
         ax.xaxis.tick_top()
         xtickslocs = np.arange(len(labels))
         ax.set_xticks(xtickslocs)
@@ -141,18 +179,18 @@ class Corrplot(object):
                 # TODO 1- set axis off
                 # 2 - set xlabels along the diagonal
                 # set colorbar either on left or bottom
-                if fill == 'lower':
+                if mode == 'lower':
                     plt.axvline(i+.5, ymin=1-ratio1, ymax=0., color='grey')
                     plt.axhline(i+.5, xmin=0, xmax=ratio2, color='grey')
-                elif fill == 'upper':
+                if mode == 'upper':
                     plt.axvline(i+.5, ymin=1 - ratio2, ymax=1, color='grey')
                     plt.axhline(i+.5, xmin=ratio1, xmax=1, color='grey')
-                else:
+                if mode in ['method', 'both']:
                     plt.axvline(i+.5, color='grey')
                     plt.axhline(i+.5, color='grey')
 
             # can probably be simplified
-            if fill == 'lower':
+            if mode == 'lower':
                 plt.axvline(-.5, ymin=0, ymax=1, color='grey')
                 plt.axvline(width-.5, ymin=0, ymax=1./width, color='grey', lw=2)
                 plt.axhline(width-.5, xmin=0, xmax=1, color='grey',lw=2)
@@ -167,7 +205,7 @@ class Corrplot(object):
                             rotation=0, horizontalalignment='right')
                 plt.axis('off')
             # can probably be simplified
-            elif fill == 'upper':
+            elif mode == 'upper':
                 plt.axvline(width-.5, ymin=0, ymax=1, color='grey', lw=2)
                 plt.axvline(-.5, ymin=1-1./width, ymax=1, color='grey', lw=2)
                 plt.axhline(-.5, xmin=0, xmax=1, color='grey',lw=2)
@@ -186,38 +224,35 @@ class Corrplot(object):
         ax.tick_params(axis='both',which='both', length=0)
 
         if colorbar:
-            from biokit.viz import imshow
-            plt.sca(axc)
-            imshow(np.transpose([np.linspace(-1,1,101)]*colorbar_width)) 
-            plt.xticks([])
-            plt.yticks([])
+            N = self.params['colorbar.N']
+            cb = plt.gcf().colorbar(self.collection, orientation='vertical', shrink=.9, 
+                boundaries= np.linspace(0,1,N), ticks=[-1,-.5,0,.5,1])
+            cb.ax.set_yticklabels([-1,-.5,0,.5,1])
 
-    def _fill_triangle(self, df, method, fill, ax):
-
+    def _add_patches(self, df, method, fill, ax, diagonal=True):
         width, height = df.shape
         labels = (df.columns)
 
+        patches = []
+        colors = []
         for x in xrange(width):
             for y in xrange(height):
-
-                if fill == 'lower':
-                    if x>y:
-                        continue
-                elif fill == 'upper':
-                    if x<y:
-                        continue
-                if self.lower and self.upper:
-                    if x==y:
-                        continue
+                if fill == 'lower' and x > y:
+                    continue
+                elif fill == 'upper' and x < y:
+                    continue
+                if diagonal is False and x==y:
+                    continue
+                datum = (df.ix[x, y] +1.)/2.
                 d = df.ix[x, y]
-                c = self.pvalues[x, y]
-                rotate = -45 if d > 0 else +45
-                cmap = self.poscm if d >= 0 else self.negcm
                 d_abs = np.abs(d)
+                #c = self.pvalues[x, y]
+                rotate = -45 if d > 0 else +45
+                #cmap = self.poscm if d >= 0 else self.negcm
                 if method in ['ellipse', 'square', 'rectangle', 'color']:
                     if method == 'ellipse':
                         func = Ellipse
-                        shape = func((x, y), width=1 * self.shrink,
+                        patch = func((x, y), width=1 * self.shrink,
                                   height=(self.shrink - d_abs*self.shrink), angle=rotate)
                     else:
                         func = Rectangle
@@ -228,23 +263,27 @@ class Corrplot(object):
                             w = 1
                             h = 1
                             offset = 0
-                        shape = func((x + offset-.5, y + offset-.5), width=w,
+                        patch = func((x + offset-.5, y + offset-.5), width=w,
                                   height=h, angle=0)
                     if self.edgecolor:
-                        shape.set_edgecolor(self.edgecolor)
-                    shape.set_facecolor(cmap(d_abs))
-                    if c > 0.05:
-                        shape.set_linestyle('dotted')
-                    ax.add_artist(shape)
+                        patch.set_edgecolor(self.edgecolor)
+                    #patch.set_facecolor(cmap(d_abs))
+                    colors.append(datum)
+                    if d_abs > 0.05:
+                        patch.set_linestyle('dotted')
+                    #ax.add_artist(patch)
+                    patches.append(patch)
                     #FIXME edgecolor is always printed
                 elif method=='circle':
-                    circle = Circle((x, y), radius=d_abs*self.shrink/2.)
+                    patch = Circle((x, y), radius=d_abs*self.shrink/2.)
                     if self.edgecolor:
-                        circle.set_edgecolor(self.edgecolor)
-                    circle.set_facecolor(cmap(d_abs))
-                    if c > 0.05:
-                        circle.set_linestyle('dotted')
-                    ax.add_artist(circle)
+                        patch.set_edgecolor(self.edgecolor)
+                    #patch.set_facecolor(cmap(d_abs))
+                    colors.append(datum)
+                    if d_abs > 0.05:
+                        patch.set_linestyle('dotted')
+                    #ax.add_artist(patch)
+                    patches.append(patch)
                 elif method in ['number', 'text']:
                     from easydev import precision
                     #FIXME 
@@ -258,19 +297,26 @@ class Corrplot(object):
                             withdash=False)
                 elif method == 'pie':
                     S = 360 * d_abs
-                    patches = [
+                    patch = [
                         Wedge((x,y), 1*self.shrink/2., -90, S-90),       
                         Wedge((x,y), 1*self.shrink/2., S-90, 360-90),
                         ]
-                    patches[0].set_facecolor(cmap(d_abs))
-                    patches[1].set_facecolor('white')
+                    #patch[0].set_facecolor(cmap(d_abs))
+                    #patch[1].set_facecolor('white')
+                    colors.append(datum)
+                    colors.append(0.5)
                     if self.edgecolor:
-                        patches[0].set_edgecolor(self.edgecolor)
-                        patches[1].set_edgecolor(self.edgecolor)
+                        patch[0].set_edgecolor(self.edgecolor)
+                        patch[1].set_edgecolor(self.edgecolor)
 
-                    ax.add_artist(patches[0])
-                    ax.add_artist(patches[1])
+                    #ax.add_artist(patch[0])
+                    #ax.add_artist(patch[1])
+                    patches.append(patch[0])
+                    patches.append(patch[1])
+        col1 = PatchCollection(patches, array=np.array(colors), cmap=self.cm)
+        ax.add_collection(col1)
 
+        self.collection = col1
 
 
 if __name__ == "__main__":
@@ -278,4 +324,3 @@ if __name__ == "__main__":
     df = pd.DataFrame(dict(( (k, np.random.random(10)) for k in ['ABCDEF'])))
     fig = Corrplot(df, None).plot()
     fig.show()
-
