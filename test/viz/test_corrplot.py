df430c3140a1ccd256cae14de3331afd4df6e164
Thomas Cokelaer <cokelaer@gmail.com>
2014-11-25 20:53:52 +0000

    fixing some issues with python3

--- test/viz/test_corrplot.py
+++ test/viz/test_corrplot.py
@@ -3,11 +3,12 @@ import string
 import pandas as pd
 import numpy as np
 
+
 class TestCorrplot(object):
 
     @classmethod
     def setup_class(klass):
-        letters = string.uppercase[0:10]
+        letters = string.ascii_uppercase[0:10]
         df = pd.DataFrame(dict(( (k, np.random.random(10)+ord(k)-65) for k in letters)))
         klass.s = corrplot.Corrplot(df.corr())
         klass.s = corrplot.Corrplot(df)
