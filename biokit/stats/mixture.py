353588757ad3a1f6f048ff0ca6f500742dd9f85b
Thomas Cokelaer <cokelaer@gmail.com>
2015-01-18 22:02:48 +0000

    update mixture (adapative)

--- biokit/stats/mixture.py
+++ biokit/stats/mixture.py
@@ -9,6 +9,8 @@ import pylab
 import seaborn as sns
 from easydev import AttrDict
 
+import criteria
+
 class Mixture(object):
     """Creates a mix of Gaussian distribution
 
@@ -52,6 +54,8 @@ class GaussianMixtureModel(object):
         assert len(params) >= 3 * self.k
         k = len(params) / 3
 
+        self.k = k
+
         pis = np.array(params[2::3])
         pis /= pis.sum()
         # !!! sum pi must equal 1 otherwise may diverge badly
@@ -66,11 +70,31 @@ class GaussianMixtureModel(object):
     def log_likelihood(self, params, sample):
         return -1 * pylab.log(self.pdf(sample, params)).sum()
 
+
 class Fitting(object):
     def __init__(self, data, model):
         self.data = np.array(data)
         self.model = model
         self.size = float(len(self.data))
+    
+    def get_guess(self):
+        params = {}
+        m = self.data.min()
+        M = self.data.max()
+        range_ = M - m
+
+        mus = [m + range_ / (self.k+1.) * i for i in range(1, self.k+1)]
+        params['mus'] = mus
+
+        sigma = range_ / float(self.k+1) / 2.
+        params['sigmas'] = [sigma] * self.k
+
+        params['pis'] = [1./self.k] * self.k
+
+        params = [ [mu,sigma,pi]  for mu,sigma,pi in 
+                    zip(params['mus'], params['sigmas'], params['pis'])]
+        params = list(pylab.flatten(params))
+        return params
 
     def plot(self, normed=True, N=1000, Xmin=None, Xmax=None, bins=50, color='red', lw=2, 
             hist_kw={'color':'#5F9EA0'}):
@@ -85,13 +109,11 @@ class Fitting(object):
         pylab.plot(X, [self.model.pdf(x, self.results.x) for x in X], color=color, lw=lw)
             
         K = len(self.results.x)
-        #TODO:
-        #print(K)
         # The PIs must be normalised
-        #for i in range(0, K/3):
-        #    mu, sigma, pi_ = self.results.x[i*3], self.results.x[i*3+1], self.results.x[i*3+2]
-        #    print(mu,sigma, pi_)
-        #    pylab.plot(X, [pi_ * pylab.normpdf(x, mu, sigma) for x in X], 'r--')
+        for i in range(0, K/3):
+            mu, sigma, pi_ = self.results.x[i*3], self.results.x[i*3+1], self.results.x[i*3+2]
+            #print(mu,sigma, pi_)
+            pylab.plot(X, [pi_ * pylab.normpdf(x, mu, sigma) for x in X], 'g--', alpha=0.5)
 
 
 class MixtureFitting(Fitting):
@@ -122,15 +144,23 @@ class MixtureFitting(Fitting):
         devtools.check_param_in_list(method, ['Nelder-Mead',
             'Powell', 'CG', 'BFGS', 'Newton-CG', 'Anneal', 'L-BFGS-B'])
         self.method = method
+        self.k = model.k
 
     def estimate(self, guess=None, method=None, maxfev=2e4, maxiter=1e3):
-        """guess is a list of parameters as expected by the model"""
+        """guess is a list of parameters as expected by the model
+        
+        
+        guess = {'mus':[1,2], 'sigmas': [0.5, 0.5], 'pis': [0.3, 0.7]  }
+
+        """
 
         if method is None:
             method = self.method
 
-        assert guess is not None
-        assert len(guess)
+        if guess is None:
+            # estimate the mu/sigma/pis from the data
+            guess = self.get_guess()
+
 
         res = minimize(self.model.log_likelihood, x0=guess, args=(self.data,), 
             method=method, options=dict(maxiter=maxiter, maxfev=maxfev))
@@ -144,6 +174,19 @@ class MixtureFitting(Fitting):
             params.append(self.results.x[(i*3+1)])
             params.append(pis[i])
         self.results.x = params 
+
+        self.results.likelihood = pylab.exp(-1*self.model.log_likelihood(params, self.data))
+        if self.results.likelihood:
+            self.results.AIC = criteria.AIC(self.results.likelihood, self.k)
+            self.results.AICc = criteria.AICc(self.results.likelihood, self.k, self.data.size)
+            self.results.BIC = criteria.BIC(self.results.likelihood, self.k, self.data.size)
+        else:
+            self.results.AIC = 1000
+            self.results.AICc = 1000
+            self.results.BIC = 1000
+
+
+
         #TODO normalise pis
 
         return res
@@ -180,12 +223,10 @@ class EM(Fitting):
         while not converged:
         # Compute the responsibility func. and new parameters
             for k in [0, 1]:
-
                 gamma[k, :] = pi_[k]*pylab.normpdf(self.data, mu[k], sig[k])/self.model.pdf(self.data, p_new)
                 N_[k] = 1.*gamma[k].sum()
                 mu[k] = sum(gamma[k]*self.data)/N_[k]
                 sig[k] = pylab.sqrt( sum(gamma[k]*(self.data - mu[k])**2)/N_[k] )
-
                 #N_[k] += 1
                 pi_[k] = N_[k] /  self.size
 
@@ -202,6 +243,38 @@ class EM(Fitting):
 
 
 
+
+class AdaptativeMixtureFitting(MixtureFitting):
+    def __init__(self, data, method='Nelder-Mead'):
+        gmm = GaussianMixtureModel(k=1) #dummy to initialise
+        super(AdaptativeMixtureFitting, self).__init__(data, gmm)
+        devtools.check_param_in_list(method, ['Nelder-Mead',
+            'Powell', 'CG', 'BFGS', 'Newton-CG', 'Anneal', 'L-BFGS-B'])
+
+    def run(self, kmin=1, kmax=6):
+        self.all_results = []
+        self.x = range(kmin, kmax+1)
+
+        for k in range(kmin, kmax+1):
+            gmm = GaussianMixtureModel(k=k)
+            self.k = k
+            self.model = gmm
+            self.estimate()
+            self.all_results.append(self.results.copy())
+
+    def plot(self, criteria='AICc'):
+        pylab.plot(self.x, [x['BIC'] for x in self.all_results], label='BIC')
+        pylab.plot(self.x, [x['AIC'] for x in self.all_results], label='AIC')
+        pylab.plot(self.x, [x['AICc'] for x in self.all_results], label='AICc')
+
+        m = np.array([this[criteria] for this in self.all_results]).min()
+        # [exp((m-this[criteria])/2) for this in amf.all_results]
+
+        pylab.axhline(m - pylab.log(0.9)*2, color='k', label='90% equi-probability', alpha=0.9)
+        pylab.axhline(m - pylab.log(0.5)*2, color='k', label='50% equi-probability', alpha=0.5)
+        pylab.axhline(m - pylab.log(0.3)*2, color='k', label='30% equi-probability', alpha=0.3)
+
+        pylab.legend()
 
 
 
