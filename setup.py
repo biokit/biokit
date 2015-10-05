2185c2e06ba097f565888ab7f12cf0f6ffb8103d
cokelaer <cokelaer@gmail.com>
2015-10-05 07:35:02 +0100

    update dependencies

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ setup(
     #package_dir  = package_dir,
 
     # distutils in rtools.package
-    install_requires = [ 'pandas', 'bioservices', 'colormap'],
+    install_requires = [ 'pandas', 'bioservices', 'colormap', 'scipy'],
 #    ext_modules=[
 #        Extension('biokit.sequence.complement', 
 #                sources=['biokit/sequence/cpp/complement.c', ],
