27a94669d4cb1e2637731835e088cc45bad9f15a
cokelaer <cokelaer@gmail.com>
2015-12-02 20:52:24 +0000

    increment version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import glob
 
 _MAJOR               = 0
 _MINOR               = 1
-_MICRO               = 0
+_MICRO               = 1
 version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 release              = '%d.%d' % (_MAJOR, _MINOR)
 
