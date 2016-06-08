e0ab89a5a2edf75eeb864902a4c905cc50d9ef6e
Thomas Cokelaer <cokelaer@gmail.com>
2016-06-08 11:51:33 +0200

    add biokit_data function

--- biokit/__init__.py
+++ biokit/__init__.py
@@ -31,4 +31,19 @@ from biokit.taxonomy import Taxonomy
 
 
 
+def biokit_data(filename, where=None):
+    """Simple utilities to retrieve data sets from biokit/data directory"""
+    import os
+    import easydev
+    sequana_path = easydev.get_package_location('biokit')
+    share = os.sep.join([sequana_path , "biokit", 'data'])
+    # in the code one may use / or \ 
+    if where:
+        filename = os.sep.join([share, where, filename])
+    else:
+        filename = os.sep.join([share, filename])
+    if os.path.exists(filename) is False:
+        raise Exception('unknown file %s' % filename)
+    return filename
+
 
