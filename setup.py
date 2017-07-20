c1000b73e494639d44696a705df3c11d0b9d99cc
Thomas Cokelaer <cokelaer@gmail.com>
2017-07-20 18:21:30 +0200

    add missing dependencies and update data package

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import glob
 
 _MAJOR               = 0
 _MINOR               = 4
-_MICRO               = 0
+_MICRO               = 2
 version              = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
 release              = '%d.%d' % (_MAJOR, _MINOR)
 
@@ -73,7 +73,7 @@ setup(
     # suds-jurko is used by bioservices so we probab ly do not need it in the
     # setup.
     install_requires = ['easydev>=0.9.11', "suds-jurko", 'pandas', 
-        'bioservices>=1.4.5', 'colormap', 'scipy',
+        'bioservices>=1.4.5', 'colormap', 'scipy', "biopython", "matplotlib",
 	"numpydoc"],
 
      # This is recursive include of data files
@@ -81,10 +81,15 @@ setup(
 
     package_data = {
         '': ['*.csv'],
-        'biokit.data' : ['*csv']
+        'biokit.data' : ['*csv'],
+        'biokit.data.converters' : ['test*']
         },
 
-
+    entry_points = {
+        'console_scripts':[
+           'converter=biokit.scripts.converter:main'
+        ]
+    }
 
 #    ext_modules=[
 #        Extension('biokit.sequence.complement', 
