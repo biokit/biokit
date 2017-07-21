6643c4a3945991f8c63529d7a138bc3d29413c9f
Thomas Cokelaer <cokelaer@gmail.com>
2017-07-21 14:04:56 +0200

    update base class for all converters

--- biokit/converters/convbase.py
+++ biokit/converters/convbase.py
@@ -1,4 +1,6 @@
 from biokit import logger
+import time
+from easydev import shellcmd
 
 
 
@@ -13,16 +15,27 @@ class ConvBase(object):
         self.args = args
         self.kargs = kargs
 
+    def set_logger_level(self, mode):
+        from biokit import biokit_debug_level
+        biokit_debug_level(mode)
+
     def execute(self, cmd):
-        import time
-        t1 = time.time()
         logger.info("CMD> " + cmd)
-        from easydev import shellcmd
         res = shellcmd(cmd, verbose=False)
+        return res
+
+    def convert(self):
+        raise NotImplementedError
+
+    def _get_name(self):
+        return type(self).__name__
+    name = property(_get_name)
+
+    def __call__(self):
+        t1 = time.time()
+        logger.info("{}> ".format(self.name))
+        self.convert()
         t2 = time.time()
         self.last_duration = t2 - t1
         logger.info("Took {} seconds ".format(t2-t1))
-        return res
-
-    
 
