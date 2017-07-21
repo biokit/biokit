0831180b495e3fd5b381dbd5717f6ac9204e0d91
Thomas Cokelaer <cokelaer@gmail.com>
2017-07-21 14:22:16 +0200

    updatinf main converter standalone

--- biokit/scripts/converter.py
+++ biokit/scripts/converter.py
@@ -25,6 +25,8 @@ import argparse
 from optparse import OptionParser
 from argparse import RawTextHelpFormatter
 
+from biokit import logger
+from biokit.converters.utils import get_format_mapper
 
 class Options(argparse.ArgumentParser):
     def  __init__(self, prog="converter"):
@@ -40,12 +42,13 @@ class Options(argparse.ArgumentParser):
 
         # options to fill the config file
         group = self.add_argument_group("Required argument")
-        group.add_argument("-i", "--input", dest="input", type=str,
-            help=("Input file in BED or BAM format. If a BAM file is "
-                 "provided, it will be converted locally to a BED file "
-                 "using genomecov, which must be installed."))
+        group.add_argument("-f", "--formats", dest="format", 
+            action="store_true", default=False,
+            help=("List available format ."))
+
 
 def main(args=None):
+    mapper = get_format_mapper()
 
     if args is None:
         args = sys.argv[:]
@@ -53,6 +56,15 @@ def main(args=None):
     user_options = Options(prog="converter")
 
     # If --help or no options provided, show the help
+    if " -f" in args or " --format" in args:
+        options = user_options.parse_args(args[1:])
+        if options.format:
+            print("Available mapping:")
+            print("==================")
+            for k in sorted(mapper):
+                print("{}: {}".format(k, mapper[k]))
+            sys.exit(0)
+
     if len(args) < 3:
         user_options.parse_args(["prog", "--help"])
     else:
@@ -60,14 +72,30 @@ def main(args=None):
         outfile = args[2]
         options = user_options.parse_args(args[3:])
 
-    inext = os.path.splitext(infile)[1]
-    outext = os.path.splitext(outfile)[1]
+    inext = os.path.splitext(infile)[-1][1:]
+    outext = os.path.splitext(outfile)[-1][1:]
 
 
+    print(inext, outext)
+    print(mapper)
     # Scanner les modules
     # get the names in lower case
     # Selectionner la classe en fonction des extensions 
 
+
+    if inext not in mapper.keys():
+        logger.critical("Input format not available in converters")
+        logger.critical("Use --formats")
+        sys.exit(1)
+
+    if outext not in mapper[inext]:
+        logger.critical("Output format not available in converters")
+        logger.critical("Use --formats")
+        sys.exit(1)
+
+    # TODO dynamic inclusion of the class and module.
+    import importlib
+    importlib.import_module("biokit.converters.{}".format(inext + "2" + outext))
 
     if infile.endswith(".fastq"):
         if outfile.endswith(".fasta"):
