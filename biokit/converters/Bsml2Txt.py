25e4e079085769011e619a92307488ce380370ec
pierreLec <plechat@pasteur.fr>
2017-09-27 10:53:05 +0200

    Update Bsml2Txt.py

--- biokit/converters/Bsml2Txt.py
+++ biokit/converters/Bsml2Txt.py
@@ -8,16 +8,7 @@ class Bsml2Txt(ConvBase):
     """
     input_ext = ['bsml']
     output_ext = '.txt'
-
-    def __init__(self, infile, outfile):
-        """.. rubric:: constructor
-        :param str filename
-        """
-        self.infile = infile
-        self.outfile = outfile
-        
-        super(Bsml2Txt, self).__init__(infile, outfile)
-        
+    
     def __call__(self):
         
         with open(self.outfile, "w") as output_handle:
