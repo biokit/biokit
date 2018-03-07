6a0cba467291d332d9a3068d72286a8c1f10590d
bryan brancotte <bryan.brancotte@pasteur.fr>
2018-03-07 16:48:09 +0100

    Ensure idempotency of load/save of fasta
    Following question ask in #65

--- test/io/test_fasta.py
+++ test/io/test_fasta.py
@@ -34,3 +34,11 @@ def test_attributes():
     s.fasta["P43403"]
     s.hist_size()
     s.df
+
+def test_idempotency():
+    f1=FASTA()
+    f1.load_fasta('P43403')
+    f1.save_fasta('/tmp/P43403.fasta')
+    f2=FASTA()
+    f2.read_fasta('/tmp/P43403.fasta')
+    assert str(f1)==str(f2)
